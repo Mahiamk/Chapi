61cf919608e90233c7702edcde2a8e28e5537a1e
Mahiamk <mahikomohammed@gmail.com>
2025-10-15 22:09:32 +0800

    refactor: update CORS origins configuration for better maintainability

--- backend/main.py
+++ backend/main.py
@@ -24,13 +24,14 @@ app = FastAPI(
 
 origins = [
     "http://localhost:3000",
-    "https://chapi-xhjf.onrender.com"
+    "https://jg7c52wx-3000.asse.devtunnels.ms",
+    "https://chapi-xhjf.onrender.com",
 ]
 
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://chapi-xhjf.onrender.com"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
